5b29c1d97bd443e5dbbd4e530601c184fdc793b2
Gabriel da Luz <gabrielbarbosaweb7@gmail.com>
2018-04-18 13:55:33 -0400

    1.1.0

--- uploadFiles7.jquery.js
+++ uploadFiles7.jquery.js
@@ -3,22 +3,168 @@
 * _Autor: Gabriel Azuaga Barbosa
 * _Descrição: Possibilita criar um campo de upload personalizado com botão e input invisvel
 *             pré carregamento de imagens em base64 e validação de arquivos e tamanho em kbytes
-* _Version: 1.0 _Criado em: 18/04/2018
+* _Version: 1.1 _Criado em: 18/04/2018
 * _Github: https://github.com/gabrielweb7/uploadFiles7.jquery.js
 **/
 (function ( $ ) {
  
     $.fn.uploadFiles7 = function( options ) {
  
-        // Default options
+		/* This */
+		var objThis = $(this);
+ 
+        /* Default options */
         var settings = $.extend({
-            name: 'John Doe',
-            color: 'orange'
+			buttonUploadById: false,
+			buttonUploadText: 'Procurar',
+			buttonRemoverById: false,
+			buttonRemoverText: 'Remover',
+            messageUploadById: false,
+            messageUploadText: 'Nenhum arquivo selecionado!',
+			onlyExtensions: false,
+			onErrorEvent: function() { },
+			onSuccess: function() { }
         }, options );
  
-        // Apply options
-        return this.append('Hello ' + settings.name + '!').css({ color: settings.color });
- 
+		/* Verifica se buttonUploadById existe*/
+		if(existeIdOrClass(settings.buttonUploadById)) { 
+			objThis.prop("style", "opacity:0;position:absolute;top:0px;bottom:0px;height:0px;width:0px;"); 
+			$(settings.buttonUploadById).html(settings.buttonUploadText);
+		}
+	
+		/* Verifica se msgUploadFotoById existe*/
+		if(existeIdOrClass(settings.messageUploadById)) { 
+			$(settings.messageUploadById).html(settings.messageUploadText);
+		}
+		
+		/* Verifica se buttonRemoverById existe*/
+		if(existeIdOrClass(settings.buttonRemoverById)) { 
+			$(settings.buttonRemoverById).hide().html(settings.buttonRemoverText);
+		}
+
+		/**  
+			Evento quando clica no objThis 
+		**/
+		$(settings.buttonUploadById).on("click", function(e) {
+			objThis.click();	
+		});
+		
+		/**  
+			Evento quando clica no buttonRemoverById 
+		**/
+		$(settings.buttonRemoverById).on("click", function(e) {
+			resetAllPlugin();
+		});
+		
+		/** 
+			Evento de quando há alteração no objThis 
+		**/
+		objThis.on("change", function(e) {
+			
+			/* Recebe dados do input */
+			var inputProp = objThis.prop("files")[0];
+			var inputPropName = inputProp["name"];
+			var inputPropType = inputProp["type"];
+			
+			/* Ver se validação de extensao está ativado */
+			if(settings.onlyExtensions) { 
+				if(isArray(settings.onlyExtensions)) {
+					if(settings.onlyExtensions.indexOf(inputPropType.toLowerCase()) > -1) {
+						settings.onSuccess(inputProp);
+					} else { 
+						/* Funcao de Erro Dinamica */
+						settings.onErrorEvent('Extensão de arquivos permitidos: '+filterArrayElementsExplode(settings.onlyExtensions,"/",1).toString().toString());
+						/* Clear value do input */
+						resetAllPlugin();
+						return false;
+					}
+				}
+			}
+			
+			/* Verifica se buttonUploadById existe*/
+			if(existeIdOrClass(settings.buttonUploadById)) { 
+				$(settings.buttonUploadById).html("Alterar");
+			}
+			
+			/* Verifica se msgUploadFotoById existe*/
+			if(existeIdOrClass(settings.messageUploadById)) { 
+				$(settings.messageUploadById).html(inputPropName);
+			}
+			
+			/* Verifica se buttonRemoverById existe*/
+			if(existeIdOrClass(settings.buttonRemoverById)) { 
+				$(settings.buttonRemoverById).show(500);
+			}
+			
+		});
+		
+		/* Funcao criada para resetar padroes do plugin */
+		function resetAllPlugin() { 
+		
+			/* Apaga value do input */
+			objThis.val("");
+			
+			/* Verifica se buttonUploadById existe e reseta configuracao padrao */
+			if(existeIdOrClass(settings.buttonUploadById)) { 
+				objThis.prop("style", "opacity:0;position:absolute;top:0px;bottom:0px;height:0px;width:0px;"); 
+				$(settings.buttonUploadById).html(settings.buttonUploadText);
+			}
+			
+			/* Verifica se msgUploadFotoById existe e reseta configuracao padrao */
+			if(existeIdOrClass(settings.messageUploadById)) { 
+				$(settings.messageUploadById).html(settings.messageUploadText);
+			}
+			
+			/* Verifica se buttonRemoverById existe e reseta configuracoes padrao */
+			if(existeIdOrClass(settings.buttonRemoverById)) { 
+				$(settings.buttonRemoverById).hide();
+			}
+		}
+		
+		/* Lê o arquivo e retorna base64 ou false */
+		function getBase64(file) {
+			var reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onload = function () {
+				return reader.result;
+			};
+			reader.onerror = function (error) {
+				return false;
+			};
+		}
+
+		/* filtro para Array - converter cada registro em case baixo */
+		function filterArrayElementsToLowerCase(value) {
+		  return value.toString().toLowerCase();
+		}
+		
+		/* filtro para Array - explode e retorna posicao selecionada */
+		function filterArrayElementsExplode(arrayVar, caractere, posicao) {
+			var newArray = [];
+			for ( var i = 0; i < arrayVar.length; i++ ) {
+				var exploded = arrayVar[i].split(caractere);
+				newArray.push(exploded[posicao]);
+			}
+			return newArray;
+		}
+		
+		
+		/* Verifica se variavel é do tipo Array */
+		function isArray(value) {
+			return value && typeof value === 'object' && value.constructor === Array;
+		};
+
+		/* Funcao que verifica se ID ou CLASS existe na pagina */
+		function existeIdOrClass(identificador) {
+			if($(identificador).length) { 
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		//return this.append('Hello ' + settings.name + '!').css({ color: settings.color });
+		
+
     };
- 
 }( jQuery ));
